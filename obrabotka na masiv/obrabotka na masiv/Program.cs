9e8297467091b78be190ecb4a46d338663d24b38
Themselfagain <sheikysmar7y@gmail.com>
2023-02-07 12:53:18 +0200

    oshe ne baca :(

--- obrabotka na masiv/obrabotka na masiv/Program.cs
+++ obrabotka na masiv/obrabotka na masiv/Program.cs
@@ -20,8 +20,29 @@ namespace obrabotka_na_masiv
         {
             foreach (var item in arr)
             {
-                Console.WriteLine(item);
+                Console.Write(item+",");
             }
+        }
+       
+        public static void EvenPrint(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                    Console.Write(arr[i] + " ");
+            }
+        }
+        public static void CountEven(int[] arr)
+        {
+            int countEven = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    countEven++;
+                }
+            }
+            Console.WriteLine("Count Even = "+countEven);
         }
     }
     class Program
@@ -29,13 +50,8 @@ namespace obrabotka_na_masiv
         static void Main(string[] args)
         {
             int count = 0;
-
-            int countEven = 0;
-
             int n = int.Parse(Console.ReadLine());
-
             int[] arr = new int[n];
-
             for (int i = 0; i < n; i++)
             {
                 arr[i] = int.Parse(Console.ReadLine());
@@ -43,6 +59,10 @@ namespace obrabotka_na_masiv
             Console.WriteLine(OptionsManager.LessThan100(arr));
 
             Console.WriteLine("max= "+OptionsManager.MaxNum(arr));
+            OptionsManager.EvenPrint(arr);
+
+            OptionsManager.CountEven(arr);
+            int even = 0;
 
             arr = OptionsManager.ReverseArray(arr);
 
@@ -53,9 +73,6 @@ namespace obrabotka_na_masiv
                 count++;
             }
             Console.WriteLine("Count = "+count);
-
-            Console.WriteLine("Count Even = " + countEven);
-
 
         }
     }
